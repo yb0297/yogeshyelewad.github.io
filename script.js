9e4005b698176831f7b8e6df3df26c7daf1afc78
Yogesh Yelewad (RA2111043010008) <97215362+yb0297@users.noreply.github.com>
2025-06-25 03:09:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const toggleBtn = document.getElementById('theme-toggle');
-
-toggleBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-  toggleBtn.textContent = document.body.classList.contains('dark-theme') ? '☀️' : '🌙';
+AOS.init({
+  duration: 1000,
+  once: true
 });
