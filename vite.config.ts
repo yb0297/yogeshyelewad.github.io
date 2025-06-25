807bc89b02bc396d634e05e6078744e6bee0bab2
Yogesh Yelewad (RA2111043010008) <97215362+yb0297@users.noreply.github.com>
2025-06-25 19:51:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/yogeshyelewad.github.io/', // 👈 Important for GitHub Pages deployment
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+plugins: [react()],
+optimizeDeps: {
+exclude: ['lucide-react'],
+},
 });
+
+
+
